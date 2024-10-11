4d1f76b47718fd03081eec426810ee1bfff85d90
renata-and <renata.androsova@gmail.com>
2024-10-11 15:09:51 +0700

    Done hw and added styles

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -4,6 +4,7 @@ import MessageSender from './message-sender/MessageSender'
 import s2 from '../../s1-main/App.module.css'
 import FriendMessage from './friend-message/FriendMessage'
 import avatar from './avatar.png'
+// import { UserType } from '../hw03/HW3'
 
 /*
 * 1 - описать тип MessageType
@@ -13,49 +14,61 @@ import avatar from './avatar.png'
 * 5 - сделать стили в соответствии с дизайном
 * */
 
+type UserType = {
+  avatar: string
+  name: string
+}
+type MessageTextType = {
+  text: string
+  time: string
+}
 // нужно создать правильный тип вместо any
-export type MessageType = any
+export type MessageType = {
+  id: number
+  user: UserType
+  message: MessageTextType
+}
 
 // структуру объекта не менять
 export const message0: MessageType = {
-    id: 0,
-    user: {
-        avatar: avatar, // можно менять
-        name: 'Some Name',  // можно менять
-    },
-    message: {
-        text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
-        time: '22:00', // можно менять
-    },
+  id: 0,
+  user: {
+    avatar: avatar, // можно менять
+    name: 'Renata',  // можно менять
+  },
+  message: {
+    text: `some textsome textsome textsome textsome textsome`, // можно менять
+    time: '22:00', // можно менять
+  },
 }
 export const friendMessage0: MessageType = {
-    id: 100,
-    user: {
-        avatar: avatar, // можно менять
-        name: 'Friend Name', // можно менять
-    },
-    message: {
-        text: 'зеркальное сообщение для тренировки css', // можно менять
-        time: '22:00', // можно менять
-    },
+  id: 100,
+  user: {
+    avatar: avatar, // можно менять
+    name: 'Katya', // можно менять
+  },
+  message: {
+    text: 'зеркальное сообщение для тренировки css', // можно менять
+    time: '22:00', // можно менять
+  },
 }
 
 const HW1 = () => {
-    return (
-        <div id={'hw1'}>
-            <div className={s2.hwTitle}>Homework #1</div>
-            <div className={s2.hw}>
-                {/*проверка отображения (не менять)*/}
-                <div>
-                    <Message message={message0} />
-                    <FriendMessage message={friendMessage0} />
-                </div>
-
-                {/*для автоматической проверки дз (не менять)*/}
-                <MessageSender M={Message} />
-            </div>
+  return (
+    <div id={'hw1'}>
+      <div className={s2.hwTitle}>Homework #1</div>
+      <div className={s2.hw}>
+        {/*проверка отображения (не менять)*/}
+        <div>
+          <Message message={message0} />
+          <FriendMessage message={friendMessage0} />
         </div>
-    )
+
+        {/*для автоматической проверки дз (не менять)*/}
+        <MessageSender M={Message} />
+      </div>
+    </div>
+  )
 }
 
 export default HW1

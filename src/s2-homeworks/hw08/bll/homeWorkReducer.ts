fa5f329d5d1cac64b30a0ffbb61f8bb083ea50b4
renata-and <renata.androsova@gmail.com>
2024-11-27 19:59:18 +0700

    HW-8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,29 @@
-import {UserType} from '../HW8'
+import { UserType } from '../HW8'
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: 'sort'; payload: 'up' | 'down' }
+  | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
+  switch (action.type) {
+    case 'sort': {
+      const newState = state.map(user => ({ ...user }))
+      if (action.payload === 'up') {
+        return newState.sort((a, b) => {
+          return a.name.localeCompare(b.name);
+        })
+      }
+      if (action.payload === 'down') {
+        return newState.sort((a, b) => {
+          return b.name.localeCompare(a.name);
+        })
+      }
+      return state
     }
+    case 'check': {
+      return state.filter(user => user.age >= 18)
+    }
+    default:
+      return state
+  }
 }

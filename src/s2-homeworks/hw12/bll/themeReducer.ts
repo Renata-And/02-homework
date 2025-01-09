aaac26c460c66c4bec33a90b7586ed2b1ca68652
renata-and <renata.androsova@gmail.com>
2025-01-09 16:23:14 +0700

    HW-12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,19 @@
+type Theme = {
+  themeId: number
+}
+
 const initState = {
-    themeId: 1,
+  themeId: 1
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // дописать
-
-        default:
-            return state
-    }
+export const themeReducer = (state = initState, action: ActionType): Theme => { // fix any
+  switch (action.type) {
+    case 'SET_THEME_ID':
+      return {...state, themeId: action.id}
+    default:
+      return state
+  }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number): ActionType => ({type: 'SET_THEME_ID', id}) // fix any
+type ActionType = { type: 'SET_THEME_ID', id: number }
